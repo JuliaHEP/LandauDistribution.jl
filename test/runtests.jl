881dfe6c574a5d4a5cf085945b34435747084ef8
Tamas Gal <himself@tamasgal.com>
2021-09-16 10:38:16 +0200

    Add some rudimentary tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using LandauDistribution
+using Distributions
 using Test
 
 @testset "LandauDistribution.jl" begin
-    # Write your tests here.
+    @test_throws ArgumentError Landau(-1, 0)
+
+    @test pdf(Landau(1, 1), 1) ≈ 0.178854161
+    @test pdf(Landau(1, 2), 3) ≈ 0.145206637
+
+    @test cdf(Landau(1, 2), 1000000000) ≈ 1
+    @test cdf(Landau(3, 4), 1000000000) ≈ 1
+    @test cdf(Landau(5, 6), 1000000000) ≈ 1
 end
